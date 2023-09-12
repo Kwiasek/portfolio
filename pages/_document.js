dac17d9be7448cc3e26f42118ce4c143a175432c
Adam Kwiasowski <kwiasekkwiasek@gmail.com>
2023-09-12 18:23:28 +0200

    Removed unused link tag

--- pages/_document.js
+++ pages/_document.js
@@ -4,12 +4,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=optional"
-          />
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
