c5e92aba2dee349efc1cbf35a00064d0ec3f23b9
Adam Kwiasowski <kwiasekkwiasek@gmail.com>
2023-09-14 11:12:50 +0200

    Updated header animation

--- app/components/header/Header.js
+++ app/components/header/Header.js
@@ -6,10 +6,10 @@ import { BsFillArrowDownCircleFill } from "react-icons/bs";
 export const Header = () => {
   return (
     <header
-      className="flex flex-col w-100 h-screen justify-center items-center text-xl leading-normal slideIn"
+      className="flex flex-col w-100 h-screen justify-center items-center text-xl leading-normal slideInDown"
       id="home"
     >
-      <span className="italic slide-in">Hello, I{"'"}m</span>
+      <span className="italic">Hello, I{"'"}m</span>
       <h1 className="font-bold text-3xl">Adam Kwiasowski</h1>
       <strong>Front End Developer</strong>
       <span className="italic">enjoy looking at my portfolio.</span>
@@ -19,20 +19,37 @@ export const Header = () => {
 
       <style jsx>
         {`
-          .slideIn {
-            animation: slide-in 1s ease-out;
-          }
-
-          @keyframes slide-in {
-            from {
-              transform: translateY(-30%);
-              opacity: 0;
-            }
-            to {
-              transform: translateY(0);
-              opacity: 1;
-            }
-          }
+        .slideInDown {
+  -webkit-animation-name: slideInDown;
+  animation-name: slideInDown;
+  -webkit-animation-duration: 1s;
+  animation-duration: 1s;
+  -webkit-animation-fill-mode: both;
+  animation-fill-mode: both;
+  }
+  @-webkit-keyframes slideInDown {
+  0% {
+  -webkit-transform: translateY(-100%);
+  transform: translateY(-100%);
+  visibility: visible;
+  }
+  100% {
+  -webkit-transform: translateY(0);
+  transform: translateY(0);
+  }
+  }
+  @keyframes slideInDown {
+  0% {
+  -webkit-transform: translateY(-100%);
+  transform: translateY(-100%);
+  visibility: visible;
+  }
+  100% {
+  -webkit-transform: translateY(0);
+  transform: translateY(0);
+  }
+  } 
+×
         `}
       </style>
     </header>
