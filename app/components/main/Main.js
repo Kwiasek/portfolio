adf4d11d0c4d40e64e2df2f6d53dfc5e313877b0
Adam Kwiasowski <kwiasekkwiasek@gmail.com>
2024-01-15 10:20:10 +0100

    Added fetching from RESTful Projects

--- app/components/main/Main.js
+++ app/components/main/Main.js
@@ -1,38 +1,66 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import { Project } from "../project/Project";
-import cvBuilder from "../../../public/CVBuilder.png";
-import rickandmorty from "../../../public/rickandmorty.png";
-import blogdotmd from "../../../public/blogdotmd.png";
+import { ThreeDots } from "react-loader-spinner";
 
 export const Main = () => {
+  const [projects, setProjects] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://restfulprojects.adaptable.app/projects", {
+      method: "GET",
+      mode: "cors",
+    })
+      .then((res) => {
+        if (res.status >= 400) {
+          throw new Error("Server error");
+        }
+        return res.json();
+      })
+      .then((data) => setProjects(data))
+      .catch((error) => {
+        setError(error);
+      });
+  }, []);
   return (
     <main id="portfolio" className="w-full flex flex-col gap-6 sm:gap-12 p-12">
       <h2 className="text-3xl font-bold text-secondary dark:text-primary">
         Portfolio
       </h2>
-      <div className="grid lg:grid-cols-3 sm:gap-6 md:grid-cols-2 grid-cols-1 gap-y-4">
-        <Project
-          name="CV Builder App"
-          description="App which helps you write simple and ready to print CV. Done using Vite, React, tailwindcss, papercss. Deployed on Vercel."
-          image={cvBuilder}
-          gitLink="https://github.com/Kwiasek/cv-builder"
-          liveLink="https://cv-builder-coral.vercel.app/"
-        />
-        <Project
-          name="Rick and Morty Memory"
-          description="Simple memory game, characters are fetched from https://rickandmortyapi.com/."
-          image={rickandmorty}
-          gitLink="https://github.com/Kwiasek/rickandmorty-memory"
-          liveLink="https://rickandmorty-memory.vercel.app/"
-        />
-        <Project
-          name="blog.md"
-          description="Blog site with little CMS where you write your posts in markdown language."
-          image={blogdotmd}
-          gitLink="https://github.com/Kwiasek/blogdotmd"
-          liveLink="https://blogdotmd.vercel.app/"
-        />
-      </div>
+
+      {error ? (
+        <div>Connection error</div>
+      ) : projects ? (
+        <div className="grid lg:grid-cols-3 sm:gap-6 md:grid-cols-2 grid-cols-1 gap-y-4">
+          {projects.map((project) => {
+            return (
+              <Project
+                key={project.name}
+                name={project.name}
+                description={project.desc}
+                gitLink={project.repoUrl}
+                liveLink={project.liveUrl}
+                image={project.img}
+              />
+            );
+          })}
+        </div>
+      ) : (
+        <div className="flex justify-center">
+          <ThreeDots
+            visible={true}
+            height="80"
+            width="80"
+            color="#9fb3db"
+            radius="9"
+            ariaLabel="three-dots-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+          />
+        </div>
+      )}
     </main>
   );
 };
--- app/components/project/Project.js
+++ app/components/project/Project.js
@@ -2,17 +2,29 @@ import React from "react";
 import Image from "next/image";
 import { GoArrowUpRight } from "react-icons/go";
 import { Roboto_Flex } from "next/font/google";
+import { Buffer } from "buffer";
 
 const roboto_flex = Roboto_Flex({ subsets: ["latin"] });
 
 export const Project = ({ name, image, description, gitLink, liveLink }) => {
   return (
     <div className="flex flex-col gap-3 sm:max-w-xl justify-center bg-primary dark:bg-secondary px-5 py-6 rounded-xl h-max min-h-full">
-      <Image
-        src={image}
-        alt="Picture representing project"
-        className="rounded-xl"
-      />
+      <div className="image-container">
+        <Image
+          src={`data:${image.contentType};base64, ${Buffer.from(
+            image.data
+          ).toString("base64")}`}
+          alt="Picture representing project"
+          className="rounded-xl"
+          sizes="100vw"
+          style={{
+            width: "100%",
+            height: "auto",
+          }}
+          width={500}
+          height={300}
+        />
+      </div>
       <strong className="text-xl text-secondary dark:text-primary">
         {name}
       </strong>
